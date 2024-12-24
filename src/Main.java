b5e2533dfcb995f47c2ef70d99d11b3eec755a87
Mpako Silas Matabane <tetelosilas@gmail.com>
2024-12-24 20:20:42 +0200

    Removed other tasks

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int fate = (int)(Math.random() * 39);
+        int fate = (int)(Math.random() * 38);
         System.out.println("Case: " + fate);
         handleFate(fate);
     }
@@ -9,7 +9,7 @@ public class Main {
         switch (fate) {
             case 0 -> System.out.println("Introduction to C# -- SoloLearn -- 1 Hours");
             case 1 -> handleMusic();
-            case 2 -> System.out.println("Ethical Hacking -- 2 Hours\nNext Session Complete Section 4 & 5");
+            case 2 -> System.out.println("Ethical Hacking\n\tNext Session Complete Section 4 & 5");
             case 3 -> System.out.println("Database TextBook Tinny College Project -- 30 Minutes");
             case 4 -> System.out.println("Computer Networks -- Textbook -- 2 Hours");
             case 5 -> handleJava();
@@ -21,7 +21,7 @@ public class Main {
             case 11 -> System.out.println("100 days - Python Project -- Complete A day");
             case 12 -> System.out.println("RESTful Web API - The Complete Guide (.NET7 API) Part 1 -- 2 Hours");
             case 13 -> System.out.println("Try to solve 1 leet code problem -- 30 minutes");
-            case 14 -> System.out.println("MySQL Udemy Project -- 3 Hours");
+            case 14 -> System.out.println("MySQL Udemy Project -- Complete a session");
             case 15 -> System.out.println("MongoDB -- 2 Hours");
             case 16 -> System.out.println("Portfolio -- 1 Hour 30 Minutes\n\tLast time you were working on the Canva Design using chatGPT - Continue from there");
             case 17 -> System.out.println("The Complete 2024 Web Development Bootcamp -- 2 Hours");
@@ -30,7 +30,7 @@ public class Main {
             case 20 -> handleInternship();
             case 21 -> handleGame();
             case 22 -> System.out.println("Solo learn - Python for Beginners -- 2 HOURS");
-            case 23 -> System.out.println("Organize your Programming books & Docs folder -- 1 hour");
+            case 23 -> System.out.println("Organize your Programming books & Docs folder -- 1 hour <<<<<<<DONE>>>>>>>");
             case 24 -> System.out.println("Find out to push a project from inteliJ to GitHub. -- 1 hour 30 minutes");
             case 25 -> System.out.println("Update CV. -- 30 minutes");
             case 26 -> handleFrontEnd();
@@ -45,7 +45,7 @@ public class Main {
             case 35 -> System.out.println("GO Through Discord and understand how it works -- 30 minutes.");
             case 36 -> System.out.println("Manage your YouTube playlists and channel subscription -- 45 minutes.");
             case 37 -> System.out.println("Exercise -- 15 minutes.");
-            case 38 -> System.out.println("Opswat Academy course  -- 1 hour.");
+
 
             default -> System.out.println("Error Somewhere");
         }
@@ -112,8 +112,8 @@ public class Main {
 
     private static void handleFrontEnd() {
         System.out.println("Find a Youtube course tutorial to learn front-end");
-        int lessonOption =  (int)(Math.random() * 6.0);
-        System.out.println("Your tutorial is option:  " + lessonOption+"\nOn the previous session the conclusion was that the I have to really lean html and css");
+        int lessonOption =  (int)(Math.random() * 7.0);
+        System.out.println("Your tutorial is option:  " + lessonOption+"\nOn the previous session the conclusion was that the I have to really lean html and css\n\tLearn: ");
         switch (lessonOption) {
             case 0 -> System.out.println("CSS");
             case 1 -> System.out.println("Bootstrap");
@@ -121,6 +121,8 @@ public class Main {
             case 3 -> System.out.println("Tailwind CSS");
             case 4 -> System.out.println("HTML wireframes and templates -- 1 hour");
             case 5 -> System.out.println("WordPress");
+            case 6 -> System.out.println("Remember to buy the Sass Course");
+
             default -> System.out.println("Something is your game code");
         }
     }
